522119711f30c53d768bdf5d3c01a9513b271b92
debo9210 <omololuadebowale1@gmail.com>
2020-04-13 02:16:51 -0400

    Initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,35 @@
+//jshint esversion:9
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({
+  robots
+}) => {
+  // if (true) {
+  //   throw new Error('Baaaaaad');
+  // }
+  return ( <
+    div > {
+      robots.map((user, i) => {
+        return ( <
+          Card key = {
+            i
+          }
+          id = {
+            robots[i].id
+          }
+          name = {
+            robots[i].name
+          }
+          email = {
+            robots[i].email
+          }
+          />
+        );
+      })
+    } <
+    /div>
+  );
+};
+
+export default CardList;
